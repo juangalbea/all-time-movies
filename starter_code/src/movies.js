7c3e9123f20e48d86ca33a402e1cc8e893e63f63
juangalbea <juangalbea@gmail.com>
2019-12-26 03:30:32 +0100

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,11 +4,11 @@
 
 const orderByYear = (array) => {
     let newArray = array.map(element => element)
-    newArray.sort((a,b) => {
+    newArray.sort((a, b) => {
         if (a.year === b.year) {
             return a.title.localeCompare(b.title)
-        }else {
-        return a.year - b.year
+        } else {
+            return a.year - b.year
         }
     });
     return newArray;
@@ -26,13 +26,56 @@ let howManyMovies = (array) => {
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 let orderAlphabetically = (array) => {
-    let newArray
+    let newArray = array.map(e => e.title);
+    newArray.sort().splice(20);
+    return newArray
 };
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
+let ratesAverage = (array) => {
+    if (array.length === 0) {
+        return 0;
+    }
+    let newArray = array.filter(elem => {
+        return typeof elem.rate !== 'undefined';
+    })
+    let number = newArray.reduce((sum, ele) => {
+        return sum += ele.rate / array.length;
+    }, 0);
+    return +number.toFixed(2);
+}
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+let dramaMoviesRate = (array) => {
+    let newArray = array.filter(movie => {
+        return movie.genre.includes('Drama');
+    });
+    let number = newArray.reduce((sum, ele) => {
+        return sum += ele.rate / newArray.length;
+    }, 0);
+    return +number.toFixed(2);
+}
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+let turnHoursToMinutes = (array) => {
+    let newArray = JSON.parse(JSON.stringify(movies));
+    newArray.map(movie => {
+        let y = movie.duration.split(' ');
+        let num = 0;
+        for (let i = 0; i < y.length; i++) {
+            if (y[i].includes('h')) {
+                num += 60 * +y[i].replace('h', '');
+            } else if (y[i].includes('min')) {
+                num += +y[i].replace('min', '');
+            }
+        }
+        movie.duration = +num
+        return movie;
+    })
+    return newArray;
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
